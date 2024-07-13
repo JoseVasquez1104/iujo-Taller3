ef1d65da68b97a89112cc46bdf220734c970a81e
JoseVasquez1104 <profefarias80@gmail.com>
2024-07-13 18:34:36 -0400

    Funcion  para calcular la combinatoria

--- main.cpp
+++ main.cpp
@@ -1,4 +1,7 @@
 #include <iostream>
+#include <cmath>
+
+using namespace std;
 
 /* Integrantes: Jose Vásquez, Adrian Aguirre, Jonas Dominguez, Luis Espinoza*/
 
@@ -9,7 +12,16 @@ Un algoritmo que calcule las permutaciones. Donde m>=n>=0
 Un algoritmo que calcule el exponente de un número.
 Se debe usar funciones*/
 
-
-int main(int argc, char** argv) {
-	return 0;
+// Función para calcular la combinatoria
+long long combinatoria(int n, int r) {
+    if (r > n || r < 0 || n < 0) {
+        cout << "Error: n >= r >= 0 no se cumple" << endl;
+        return -1;
+    }
+    long long resultado = 1;
+    for (int i = 1; i <= r; i++) {
+        resultado = resultado * (n - i + 1) / i;
+    }
+    return resultado;
 }
+
