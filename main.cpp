3ca028b1b797d02a13cb95ae76f9c0c5c3c69b52
JoseVasquez1104 <profefarias80@gmail.com>
2024-07-13 18:31:27 -0400

    Poniendo nombre de integrantes y el enunciado

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+/* Integrantes: Jose Vásquez, Adrian Aguirre, Jonas Dominguez, Luis Espinoza*/
+
+/* Enunciado: Realice un programa que contenga un menú de opciones y en cada opción ejecutara los
+siguientes requerimientos.
+• Un algoritmo que calcule la combinatoria. Donde n>=r>=0
+Un algoritmo que calcule las permutaciones. Donde m>=n>=0
+Un algoritmo que calcule el exponente de un número.
+Se debe usar funciones*/
+
+
+int main(int argc, char** argv) {
+	return 0;
+}
